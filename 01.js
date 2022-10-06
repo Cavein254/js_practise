bfd504811904a768d221e2a0681b1a8f073d2a09
johnmuthua <caveomuthama@gmail.com>
2022-10-06 14:14:54 +0300

    using while loop

---
+++ 01.js
@@ -0,0 +1,34 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+const a = new Node("a");
+const b = new Node("b");
+const c = new Node("c");
+const d = new Node("d");
+const e = new Node("e");
+const f = new Node("f");
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+
+const depfthFirstValues = (root) => {
+  if (root === null) return [];
+  const stack = [root];
+  while (stack.length > 0) {
+    const current = stack.pop();
+    console.log(stack);
+
+    if (current.left) stack.push(current.left);
+    if (current.right) stack.push(current.right);
+  }
+};
+
+depfthFirstValues(a);
---
+++ trees.md
@@ -0,0 +1,13 @@
+# Trees
+
+Tree have node and edges. Root is the node that has no parents. Leaf node has no children.
+
+## Binary Tree
+
+- This is a tree in which every node has at most two children per node.
+- One path between the root and any root
+- Only one root node
+
+### Depth first reversal
+
+starts to the top and goes deeper within the node.
